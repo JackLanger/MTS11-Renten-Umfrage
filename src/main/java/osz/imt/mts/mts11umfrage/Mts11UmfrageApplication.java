ca93c7161d7bd73e58314f1b7e09d61abcb3aa06
Langer, Jacek <jacek.langer@tu-darmstadt.de>
2022-10-10 11:26:02 +0200

    initial

---
+++ src/main/java/osz/imt/mts/mts11umfrage/Mts11UmfrageApplication.java
@@ -0,0 +1,14 @@
+package osz.imt.mts.mts11umfrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mts11UmfrageApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(Mts11UmfrageApplication.class, args);
+  }
+
+}
---
+++ src/test/java/osz/imt/mts/mts11umfrage/Mts11UmfrageApplicationTests.java
@@ -0,0 +1,14 @@
+package osz.imt.mts.mts11umfrage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mts11UmfrageApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
