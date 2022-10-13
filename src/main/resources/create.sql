0252d3a8c922929dabcc62deca99083a5e65953a
Langer, Jacek <jacek.langer@tu-darmstadt.de>
2022-10-13 13:58:15 +0200

    chore: added drop script and adjust app.prop

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -2,25 +2,17 @@ CREATE TABLE t_question
 (
     p_question_id int NOT NULL,
     question_text varchar(255),
-    type_type_id  int,
+    type          int,
     CONSTRAINT pk_t_question PRIMARY KEY (p_question_id)
 )
 GO
 
-CREATE TABLE t_question_type
-(
-    type_id int NOT NULL,
-    value   varchar(255),
-    CONSTRAINT pk_t_question_type PRIMARY KEY (type_id)
-)
-GO
 
 CREATE TABLE t_question_answer
 (
     id                     int IDENTITY (1, 1) NOT NULL,
     question_p_question_id int,
     answer_option          varchar(255),
-    html_element           varchar(255),
     CONSTRAINT pk_t_question_answer PRIMARY KEY (id)
 )
 GO
@@ -30,20 +22,15 @@ CREATE TABLE t_user_answers
     id                 int IDENTITY (1, 1) NOT NULL,
     question_answer_id int,
     user_answer_value  varchar(255)        NOT NULL,
+    f_user_session_id  uniqueidentifier,
     CONSTRAINT pk_t_user_answers PRIMARY KEY (id)
 )
 GO
 
-
-ALTER TABLE t_question
-    ADD CONSTRAINT FK_T_QUESTION_ON_TYPE_TYPE FOREIGN KEY (type_type_id) REFERENCES t_question_type (type_id)
+ALTER TABLE t_user_answers
+    ADD CONSTRAINT FK_T_USER_ANSWERS_ON_QUESTION_ANSWER FOREIGN KEY (question_answer_id) REFERENCES t_question_answer (id)
 GO
 
 ALTER TABLE t_question_answer
     ADD CONSTRAINT FK_T_QUESTION_ANSWER_ON_QUESTION_P_QUESTION FOREIGN KEY (question_p_question_id) REFERENCES t_question (p_question_id)
-GO
-
-
-ALTER TABLE t_user_answers
-    ADD CONSTRAINT FK_T_USER_ANSWERS_ON_QUESTION_ANSWER FOREIGN KEY (question_answer_id) REFERENCES t_question_answer (id)
 GO
---
+++ src/main/resources/drop.sql
@@ -0,0 +1,8 @@
+DROP TABLE t_question
+GO
+
+DROP TABLE t_question_answer
+GO
+
+DROP TABLE t_user_answers
+GO
