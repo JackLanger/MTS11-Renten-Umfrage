f0e75146e370e2275bcc7938be61edac10910452
Langer, Jacek <jacek.langer@tu-darmstadt.de>
2022-10-21 09:24:55 +0200

    refactor: cleanup removed redundant code

--- src/main/java/osz/imt/mts/mts11umfrage/service/UserManagementService.java
+++ src/main/java/osz/imt/mts/mts11umfrage/service/UserManagementService.java
@@ -1,21 +1,20 @@
 package osz.imt.mts.mts11umfrage.service;
 
 import java.util.UUID;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
  * UserData management service to collect user_data session id and store in memory while user_data
- * not yet finished with the survey. UserData id can be stored or fetched via the cookies and or
- * cached in local memory.
+ * not yet finished with the survey. Further should manage data in a in memory store e.g. Redis and
+ * verify that a given user has already finished or left the survey. if user left a survey they
+ * should pick up where they left, therefore the index of the answer should be provided back to the
+ * browser.
  */
 @Service
 public class UserManagementService {
   // TODO(Moritz): 16.10.2022 implement
-
-  @Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+  
   @Cacheable("UUID")
   public UUID getUserId() {
     // TODO(...): 16.10.2022 Implement
