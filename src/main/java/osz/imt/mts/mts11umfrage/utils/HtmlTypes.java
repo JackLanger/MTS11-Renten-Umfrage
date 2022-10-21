caad4fb25579d3894880f4f9693a3d312c1ed328
Langer, Jacek <jacek.langer@tu-darmstadt.de>
2022-10-21 09:27:42 +0200

    chore: helper class for constants

---
+++ src/main/java/osz/imt/mts/mts11umfrage/utils/HtmlTypes.java
@@ -0,0 +1,20 @@
+package osz.imt.mts.mts11umfrage.utils;
+
+/**
+ * Html types.
+ *
+ * <p>Created by: Jack</p>
+ * <p>Date: 21.10.2022</p>
+ */
+public class HtmlTypes {
+
+  /**
+   * Span element for html views.
+   */
+  public static final String TEXT = "span";
+  /**
+   * Image element for html views.
+   */
+  public static final String IMAGE = "a";
+
+}
