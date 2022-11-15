34bca9554bb56aa3ceb5cd79a98394fd0a2e683b
Langer, Jacek <jacek.langer@tu-darmstadt.de>
2022-11-15 20:53:11 +0100

    chore: utils class for paths

---
+++ src/main/java/osz/imt/mts/mts11umfrage/utils/PathUtils.java
@@ -0,0 +1,17 @@
+package osz.imt.mts.mts11umfrage.utils;
+
+/**
+ * .
+ *
+ * <p>Created by: Jack</p>
+ * <p>Date: 15.11.2022</p>
+ */
+public final class PathUtils {
+
+  public static final String DOWNLOAD_PATH = "/bin/venv/media.python/";
+
+  private PathUtils() {
+    //
+  }
+
+}
